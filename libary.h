283a29ccfb2ef8c7af3ec53be9332d71e1223dcb
varshnie <125456267+varshnie@users.noreply.github.com>
2023-02-15 16:32:14 +0530

    This is the header file

--- libary.h
+++ libary.h
@@ -1,1 +1,9 @@
+char* stringcat(const char* str1, const char* str2);
+char* stringcopy(char* destination, char* source);
+char* strncopy(char *dest, const char *src, size_t n);
+char* strchar(char* str, char ch);
 char* strtoken(char* str, char sep);
+char strrev(char* str);
+void* memcopy(void* dest, const void* src, int n) ;
+void* memmovee(void* dest, const void* src, int n);
+
