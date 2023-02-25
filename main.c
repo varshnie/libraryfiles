d683752d6c4ca43f80b6daccfd0b7d13428f4a06
varshnie <125456267+varshnie@users.noreply.github.com>
2023-02-26 01:29:25 +0530

    main programs

--- main.c
+++ main.c
@@ -6,47 +6,46 @@
 int main() 
 
 {
-    char str1[] = "Hello, ";
-    char str2[] = "world!";
-    char* result = stringcat(str1, str2);
-    printf("%s\n", result);
-    free(result);
+    char a[50] = "Hello, ";
+    char b[50] = "world!";
+    stringcat(a,b);
+    printf("%s",a);
+    
+    
+    
+    char c[50] = "Hello, ";
+    char d[50] = "world!";
+    stringcopy(c,d);
+    printf("%s",c);
+
   
+
   
-    char a[]="hello";
-    char* b=NULL;
-    b=stringcopy(b,a);
-    printf("%s",b);
-  
-  
-     char a[]="this is a ball";
-     char b[50];
-     strncopy(b,a,7);
-     printf("%s",b);
+     char e[]="this is a ball";
+     char f[50];
+     strncopy(f,e,7);
+     printf("%s",f);
      
   
-    char str[100];
-    printf("Enter a string: ");
-    scanf("%[^\n]s",str);
-    strrev(str);
-    printf("Reversed string: %s\n", str);
+    char g[]="This is a ball";
+    strrev(g);
+    printf(" %s", g);
     
   
-  
-    char str[]="hello world";
-    char a=' ';
-    char* res=strtoken(str,a);
+    char h[]="hello world";
+    char i=' ';
+    char* res=strtoken(h,i);
     printf("%s",res);
+    
   
-    char str[] = "hello world";
-    char* result = strchar(str, 'o');
+    char j[] = "hello world";
+    char* result = strchar(j, 'o');
     printf("Result: %s\n", result);
-    free(result);
+   
   
     char str1[20] = "Hello, world!";
     char str2[20];
-    int n = sizeof(str1);
-    memcopy(str2, str1, n);
+    memcopy(str2, str1, sizeof(str1));
     printf("Copied string: %s\n", str2);
     
   
